e652d97bddf64a1252f7b7c9715381f8df98eb35
Aryeh Greenberg <13561405+arr00@users.noreply.github.com>
2021-03-31 16:12:40 -0400

    Initial Commit

---
+++ SocialDistanceBracelet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialDistanceBracelet
+//
+//  Created by Aryeh Greenberg on 3/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
